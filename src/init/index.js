4901aceef53b0fb6655b1cbb260bab9d68ce4322
Lorenzo Mussi <lorenzo.mussi@docomodigital.com>
2018-10-12 12:46:41 +0200

    fix linting

--- src/init/index.js
+++ src/init/index.js
@@ -1,7 +1,7 @@
 /**
  * @memberof JsLogger
  * @function init
- * @description Enable or disable logger for production environment. If set to false, only warnings 
+ * @description Enable or disable logger for production environment. If set to false, only warnings
  * and error will be logged.
  * @param {Object} options (see attributes below)
  * @param {String} [options.enable=false] enable/disable log
